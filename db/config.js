7b21e952716e29af9eb80934a71eef04a3d77588
dufufeisdu <fufei.du@gmail.com>
2017-04-27 15:09:16 -0700

    (feat) set up the database and table schema

--- db/config.js
+++ db/config.js
@@ -1,12 +1,20 @@
 var serverUrl = '127.0.0.1';
+var connection = {
+  host: serverUrl,
+  user: 'root',
+  password: ''
+};
 var knex = require('knex')({
-  client: 'sql',
-  connection: {
-    host: serverUrl,
-    user: 'root',
-    password: '',
-    database: './database.sql'
-  },
+  client: 'mysql',
+  connection: connection,
+  useNullAsDefault: true
+});
+knex.raw('CREATE DATABASE IF NOT EXISTS trendNewsDB')
+  .then(function() {});
+connection.database = 'trendNewsDB';
+knex = require('knex')({
+  client: 'mysql',
+  connection: connection,
   useNullAsDefault: true
 });
 var db = require('bookshelf')(knex);
@@ -16,23 +24,37 @@ db.knex.schema.hasTable('trends').then(function(exists) {
       trend.increments('id').primary();
       trend.string('name');
       trend.timestamps();
+    }).then(function(table) {
       console.log('created Table trends');
-    }).createTable('weeks', function(week) {
+    });
+  }
+});
+db.knex.schema.hasTable('trends').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('weeks', function(week) {
       week.increments('id').primary();
       week.string('startDate');
       week.integer('intensity');
-      week.integer('trendId').references('trends.id');
+      week.integer('trendId').unsigned();
+      week.foreign('trendId').references('trends.id');
+    }).then(function(table) {
       console.log('created Table weeks');
-    }).createTable('stories', function(story) {
+    });
+  }
+});
+db.knex.schema.hasTable('trends').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('stories', function(story) {
       story.increments('id').primary();
       story.string('articleName', 500);
       story.string('mediaUrl', 200);
       story.string('url', 500);
       story.string('previewText', 1000);
-      story.interger('weeksId').references('weeks.id');
+      story.integer('weekId').unsigned();
+      story.foreign('weekId').references('weeks.id');
+    }).then(function(table) {
       console.log('created Table stories');
     });
   }
 });
-
 module.exports = db;
